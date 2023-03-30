9a935e023fe1ea71e65dbc44fa2801a386eaa786
Daim <daimabbask05@gmail.com>
2023-03-30 21:00:40 +0500

    Initial Commit

---
+++ Card Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card Game
+//
+//  Created by Daim Abbas Kazmi on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
